fe118162307ae917fbf81e8391ec0a3ab528ac73
iancooper <ian_hammond_cooper@yahoo.co.uk>
2020-08-24 20:35:18 +0100

    test fails with three cells...now we need to write some code

--- conway/tests/test_game_of_life.py
+++ conway/tests/test_game_of_life.py
@@ -43,3 +43,41 @@ def test_single_cell_with_no_neighbours_dies():
     generation_one = seed.tick()
 
     assert generation_one == expected_generation_one
+
+def test_two_adjacent_cells_with_insufficient_neighbours_die():
+    """ Just one cell is live, and it has no neighbours, so must die"""
+
+    seed = Board(0, (3, 3), [
+        ['.', '*', '.'],
+        ['.', '*', '.'],
+        ['.', '.', '.']
+    ])
+
+    expected_generation_one = Board(1, (3, 3), [
+        ['.', '.', '.'],
+        ['.', '.', '.'],
+        ['.', '.', '.']
+    ])
+
+    generation_one = seed.tick()
+
+    assert generation_one == expected_generation_one
+
+def test_two_adjacent_cells_with_insufficient_neighbours_die():
+    """ Just one cell is live, and it has no neighbours, so must die"""
+
+    seed = Board(0, (3, 3), [
+        ['.', '*', '.'],
+        ['.', '*', '.'],
+        ['.', '*', '.']
+    ])
+
+    expected_generation_one = Board(1, (3, 3), [
+        ['.', '.', '.'],
+        ['.', '*', '.'],
+        ['.', '.', '.']
+    ])
+
+    generation_one = seed.tick()
+
+    assert generation_one == expected_generation_one
